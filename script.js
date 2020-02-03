430163656c6b5ca37d7772fa640936a2a505f941
Daniel Droppa <danwdroppa@gmail.com>
2020-02-02 20:28:01 -0500

    set prompt and alert messeges

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-var choices = {
-  lowercase:["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-   function randLower() {
-    Math.floor(Math.random());
-  },
+// var choices = {
+//   lowercase:["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+//    function randLower() {
+//     Math.floor(Math.random());
+//   },
   
-  uppercase:["A","B","C","D","E","F","G","h","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","w","X","Y","Z"],
-  nums:[1,2,3,4,5,6,7,8,9,0],
-  symbol: ["!","@","#","$","%","^","&","*","(",")","[","]","|"],
-}
+//   uppercase:["A","B","C","D","E","F","G","h","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","w","X","Y","Z"],
+//   nums:[1,2,3,4,5,6,7,8,9,0],
+//   symbol: ["!","@","#","$","%","^","&","*","(",")","[","]","|"],
+// }
 
 
 
