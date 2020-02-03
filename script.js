a11fed1744d3f378e08af8d5487d8f9b70723677
Daniel Droppa <danwdroppa@gmail.com>
2020-02-03 12:33:37 -0500

    removed rand g in js code

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function writePassword() {
       if (randomNumber === 0) {
         function makeidlower(length) {
           var result = '';
-          var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+          var characters = 'abcdefghijklmnopqrstuvwxyz';
           var charactersLength = characters.length;
           for (var i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -104,12 +104,12 @@ function writePassword() {
 
 
 
-g
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 
 
